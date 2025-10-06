baa66b6c2da539c86fea1c3e30848ca22fecc52d
alainkatayi <alainkatayi01@gmail.com>
2025-10-06 17:37:29 +0100

    Implémentation de la méthode index dans le controller Participants

---
+++ app/Http/Controllers/ParticipantsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Participants;
+use App\Http\Requests\StoreParticipantsRequest;
+use App\Http\Requests\UpdateParticipantsRequest;
+use App\Models\Formation;
+
+class ParticipantsController extends Controller
+{
+
+    public function index()
+    {
+        $participants = Participants::all();
+        return response()->json([
+            "Participants" => $participants
+        ]);
+    }
+
+}
